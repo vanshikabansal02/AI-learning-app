94b445906f16f6c58f2cb9c3e3d467b3fa043f2c
vanshika bansal <166854639+vanshikabansal02@users.noreply.github.com>
2026-09-02 16:01:30 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,7 @@ const userSchema=new mongoose.Schema({
         type:String ,
         required:true,
         unique:true,
-        trin:true,
+        trim:true,
     },
     email:{
         type:String,
